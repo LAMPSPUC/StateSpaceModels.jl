b28654f9cedfd644ae13b46bd7313c86e4825b90
rsaavedra <raphael.saavedra93@gmail.com>
2019-05-21 12:28:19 -0300

    including test for structural model with exogenous variables

--- test/test_statespace.jl
+++ test/test_statespace.jl
@@ -1,12 +1,27 @@
 # Tests
 @testset "Strutural model tests" begin
 
-    @testset "Constant signal test" begin
+    @testset "Constant signal with basic structural model" begin
         y = ones(15)
-        model = StructuralModel(y, 17)
+        model = StructuralModel(y, 2)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
         @test isa(model, StateSpaceModels.BasicStructuralModel)
+
+        ss = statespace(model; nseeds = 3)
+
+        @test all(ss.param.sqrtH .< 1e-6)
+        @test all(ss.param.sqrtQ .< 1e-6)
+    end
+
+    @testset "Constant signal with exogenous variables" begin
+        y = ones(15)
+        X = randn(15, 2)
+
+        model = StructuralModel(y, 2; X = X)
+
+        @test isa(model, StateSpaceModels.StateSpaceModel)
+        @test isa(model, StateSpaceModels.StructuralModelExogenous)
 
         ss = statespace(model; nseeds = 3)
 
