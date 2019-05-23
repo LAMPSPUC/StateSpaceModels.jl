593fbaade9435f157a2e3e9053e9cdf62a80c7c1
rsaavedra <raphael.saavedra93@gmail.com>
2019-05-23 11:39:47 -0300

    fixing broken tests

--- test/test_statespace.jl
+++ test/test_statespace.jl
@@ -3,10 +3,9 @@
 
     @testset "Constant signal with basic structural model" begin
         y = ones(15)
-        model = StructuralModel(y, 2)
+        model = structuralmodel(y, 2)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
-        @test isa(model, StateSpaceModels.BasicStructuralModel)
 
         ss = statespace(model; nseeds = 3)
 
@@ -18,10 +17,9 @@
         y = ones(15)
         X = randn(15, 2)
 
-        model = StructuralModel(y, 2; X = X)
+        model = structuralmodel(y, 2; X = X)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
-        @test isa(model, StateSpaceModels.StructuralModelExogenous)
 
         ss = statespace(model; nseeds = 3)
 
