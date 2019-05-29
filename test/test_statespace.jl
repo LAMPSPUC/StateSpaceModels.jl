c8f120d360e49a95bf0c586bb91f3df4bb5df8ee
rsaavedra <raphael.saavedra93@gmail.com>
2019-05-29 12:23:28 -0300

    updating simulation

--- test/test_statespace.jl
+++ test/test_statespace.jl
@@ -2,10 +2,11 @@
 @testset "Strutural model tests" begin
 
     @testset "Constant signal with basic structural model" begin
-        y = ones(15)
+        y = ones(30)
         model = structuralmodel(y, 2)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
+        @test model.mode == "time-invariant"
 
         ss = statespace(model)
 
@@ -22,6 +23,7 @@
         model = structuralmodel(y, 2; X = X)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
+        @test model.mode == "time-variant"
 
         ss = statespace(model)
 
@@ -37,10 +39,31 @@
         logAP = log.(Vector{Float64}(AP[:Passengers]))
 
         model = structuralmodel(logAP, 12)
-        ss = statespace(model)
 
         @test isa(model, StateSpaceModels.StateSpaceModel)
+        @test model.mode == "time-invariant"
+
+        ss = statespace(model)
+
         @test isa(ss, StateSpaceModels.StateSpace)
+
+    end
+
+end
+
+@testset "Simulation tests" begin
+
+    @testset "Affine series simulation" begin
+
+        y = collect(1.:30)
+        model = structuralmodel(y, 2)
+        ss = statespace(model)
+        sim = simulate(ss, 20, 100)
+        media_sim = mean(sim, dims = 3)[1, :]
+
+        @test size(sim) == (1, 20, 100)
+        @test media_sim ≈ collect(31.:50) rtol = 1e-3
+        @test var(sim[1, end, :]) < 1e-6
 
     end
 
