e049e9ef6b5052a8ab882836a131975774462cdd
Raphael Saavedra <raphael.saavedra93@gmail.com>
2019-06-03 12:53:30 -0300

    removing @info from constructor

--- src/models.jl
+++ src/models.jl
@@ -130,8 +130,6 @@ function lineartrendmodel(y::VecOrMat{Typ}) where Typ <: AbstractFloat
     y = y[:, :]
     n, p = size(y)
 
-    @info("Creating linear trend model with $p endogenous variables.")
-
     m = 2*p
     r = 2*p
 
