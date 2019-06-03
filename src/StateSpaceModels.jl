7f8ab2c6e9032eefb738e91be5686167f50b53a9
rsaavedra <raphael.saavedra93@gmail.com>
2019-06-03 10:19:19 -0300

    adding verbose parameter

--- src/StateSpaceModels.jl
+++ src/StateSpaceModels.jl
@@ -13,21 +13,32 @@ include("estimation.jl")
 include("kalman.jl")
 include("simulation.jl")
 
-function statespace(model::StateSpaceModel; nseeds::Int = 3)
+function statespace(model::StateSpaceModel; nseeds::Int = 3, verbose::Int = 1)
 
-    @info("Starting state-space model estimation...")
+    if !(verbose in [0, 1, 2])
+        @warn("Incorrect verbose value input (should be 0, 1, or 2): switching to default value 1")
+        verbose = 1
+    end
+
+    if verbose > 0
+        @info("Starting state-space model estimation...")
+    end
 
     # Maximum likelihood estimation
-    param = estimate_statespace(model, nseeds)
+    param = estimate_statespace(model, nseeds; verbose = verbose)
 
-    @info("End of estimation.")
-    @info("Starting filtering and smoothing...")
+    if verbose > 0
+        @info("End of estimation.")
+        @info("Starting filtering and smoothing...")
+    end
 
     # Kalman filter and smoothing
     kfilter, U2star, K = sqrt_kalmanfilter(model, param.sqrtH, param.sqrtQ)
     smoothedstate = sqrt_smoother(model, kfilter, U2star, K)
 
-    @info("Filtering and smoothing completed.")
+    if verbose > 0
+        @info("Filtering and smoothing completed.")
+    end
 
     output = StateSpace(model, smoothedstate, param, kfilter)
 
