00ea72ea006d496cb6daa2a0802ffa1be5a51c8d
mariohsouto <mariohsouto@gmail.com>
2019-06-07 11:52:16 -0300

    timing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,18 @@
+
+path = joinpath(dirname(@__FILE__), "..", "..")
+push!(Base.LOAD_PATH, path)
+
 using Test, StateSpaceModels, Statistics, CSV
+
+using TimerOutputs
+
 # Run tests
-include("test_userdefined.jl")
-include("test_locallevel.jl")
-include("test_lineartrend.jl")
-include("test_statespace.jl")
-include("test_utils.jl")
+
+reset_timer!()
+@timeit "section1" include("test_userdefined.jl")
+@timeit "section2" include("test_locallevel.jl")
+@timeit "section3" include("test_lineartrend.jl")
+@timeit "section4" include("test_statespace.jl")
+@timeit "section5" include("test_utils.jl")
+
+print_timer()
