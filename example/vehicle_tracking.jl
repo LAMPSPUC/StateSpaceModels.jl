51aba5a8a1f25110fc2c7609b31044aa96731495
mariohsouto <mariohsouto@gmail.com>
2019-07-02 18:17:06 -0300

    minor change in control

--- example/vehicle_tracking.jl
+++ example/vehicle_tracking.jl
@@ -5,7 +5,7 @@ using StateSpaceModels
 using Distributions, LinearAlgebra, Plots, Random
 
 # Number of observations
-n = 100
+n = 300
 # State dimension (2d position + 2d speed)
 m = 4
 # Measurements dimension (2d noisy position)
@@ -14,12 +14,12 @@ p = 2
 q = 2
 
 # Damping ratio
-damping = .05
+ρ = .05
 # Time delta
 Δ = 1.
 
 # State transition matrix
-T = kron(Matrix{Float64}(I, p, p), [1. (1. - damping * Δ / 2.) * Δ; 0. (1. - damping * Δ)])
+T = kron(Matrix{Float64}(I, p, p), [1. (1. - ρ * Δ / 2.) * Δ; 0. (1. - ρ * Δ)])
 # Input matrix
 R = kron(Matrix{Float64}(I, p, p), [.5 * Δ^2.; Δ])
 # Output (measurement) matrix
@@ -30,7 +30,7 @@ Q = .5 * Matrix{Float64}(I, q, q)
 η = MvNormal(zeros(q), Q)
 
 # Generate random measurement noise
-H = 1.5 * Matrix{Float64}(I, p, p)
+H = 2. * Matrix{Float64}(I, p, p)
 ε = MvNormal(zeros(p), H)
 
 # Simulate vehicle trajectory
@@ -48,5 +48,5 @@ model = StateSpaceModel(y, Z, T, R)
 ss = statespace(model)
 
 plot(ss.smoother.alpha[:, 1], ss.smoother.alpha[:, 3], title="Vehicle tracking", lw=3, label="Estimate position")
-plot!(y[:, 1], y[:, 2], label="Measured position", lw=3)
+plot!(y[:, 1], y[:, 2], label="Measured position", line=:scatter, lw=3, markeralpha=0.2, color=:blue)
 plot!(α[:, 1], α[:, 3], label="True position", lw=3)
