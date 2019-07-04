85446a7585653da518765e11be7fcf0ef9676b26
rsaavedra <raphael.saavedra93@gmail.com>
2019-07-04 15:59:21 -0300

    updating example docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,22 @@
 using Documenter, StateSpaceModels
 
+cd("/home/rsaavedra/Dropbox/Research/StateSpaceModels.jl/docs")
+
 makedocs(
     modules = [StateSpaceModels],
     doctest  = false,
     clean    = true,
     format   = Documenter.HTML(),
     sitename = "StateSpaceModels.jl",
-    authors = "Raphael Saavedra, Mario Souto and Guilherme Bodin.",
+    authors = "Raphael Saavedra, Guilherme Bodin, and Mario Souto",
     pages = [
         "Home" => "index.md",
         "manual.md",
-        "examples.md"
+        "examples.md",
         "reference.md"
     ]
 )
 
-deploydocs(
-    repo = "github.com/LAMPSPUC/StateSpaceModels.jl.git",
-)
+# deploydocs(
+#     repo = "github.com/LAMPSPUC/StateSpaceModels.jl.git",
+# )
