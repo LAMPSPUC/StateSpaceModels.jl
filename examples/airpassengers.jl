1309129fda3958e2d449a4af654e448b07f3f168
rsaavedra <raphael.saavedra93@gmail.com>
2019-07-04 18:40:15 -0300

    updating airline example

--- examples/airpassengers.jl
+++ examples/airpassengers.jl
@@ -1,43 +1,30 @@
 using CSV, StateSpaceModels, Plots, Statistics, Dates
 
-#load the AirPassengers dataset
+# Load the AirPassengers dataset
 AP = CSV.read("AirPassengers.csv")
 
-#Take the log of the series
-logAP = log.(Array{Float64}(AP[:Passengers]))
+# Take the log of the series
+logAP = log.(Vector{Float64}(AP[:Passengers]))
 
-#Visualize the data
-p1 = plot(AP[:Date], logAP, label = "AirPassengers timeseries", size = (1000, 500))
+# Plot the data
+p1 = plot(AP[:Date], logAP, label = "Log-airline passengers", legend = :topleft, color = :black)
 
-#Define its seasonality 
-s = 12
+# Specify the state-space model
+model = structural(logAP, 12)
 
-#Estimate a StateSpace Structure
-ss = statespace(logAP, s)
+# Estimate the state-space model
+ss = statespace(model)
 
-#Analyze its decomposition in seasonal and trend
-p2 = plot(AP[:Date], ss.state.seasonal, label = "AirPassengers seasonal", size = (1000, 500))
-p3 = plot(AP[:Date], ss.state.trend, label = "AirPassengers trend", size = (1000, 500))
+# Analyze its decomposition in trend and seasonal
+p2 = plot(AP[:Date], [ss.smoother.alpha[:, 1] ss.smoother.alpha[:, 3]], layout = (2, 1),
+            label = ["Trend component" "Seasonal component"], legend = :topleft)
 
-#Simulate 100 scenarios, 20 steps ahead
-num_scenarios = 100
-num_steps_ahead = 60
-simulation = simulate(ss, num_steps_ahead, num_scenarios)
+# Forecast 24 months ahead
+N = 24
+pred, dist = forecast(ss, N)
 
-#Define simulation dates
+# Define forecasting dates
 firstdate = AP[:Date][end] + Month(1)
-newdates = collect(firstdate:Month(1):firstdate + Month(num_steps_ahead - 1))
+newdates = collect(firstdate:Month(1):firstdate + Month(N - 1))
 
-#Evaluating the mean of the forecast and its quantiles
-simulation_mean = mean(simulation, dims = 3)[1, :, :]
-
-n = length(logAP)
-nmonths = length(simulation[1, :, 1])
-simulation_q05 = zeros(nmonths)
-simulation_q95 = zeros(nmonths)
-for t = 1:nmonths
-    simulation_q05[t] = quantile(simulation[1, t, :], 0.05)
-    simulation_q95[t] = quantile(simulation[1, t, :], 0.95)
-end
-
-plot!(p1, newdates, [simulation_q05, simulation_mean, simulation_q95], labels = ["5% quantile", "mean", "95% quantile"])
+p3 = plot!(p1, newdates, pred, label = "Forecast")
