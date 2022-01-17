826c8331edab3389885a58f9ca5cb5b45a597780
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2022-01-17 12:22:59 -0300

    Update

--- test/models/linear_regression.jl
+++ test/models/linear_regression.jl
@@ -6,7 +6,7 @@
         X = rand(100, 5)
         y = rand(100)
         model = LinearRegression(X, y)
-        fit!(model; save_hyperparameter_distribution_results=false)
+        fit!(model; save_hyperparameter_distribution=false)
         β = X \ y
         for i in 1:size(X, 2)
             @test get_constrained_value(model, "β_$i") .≈ β[i] atol = 1e-5
@@ -27,7 +27,7 @@
         10.0
     ]
     model = LinearRegression(X, y)
-    fit!(model; save_hyperparameter_distribution_results=false)
+    fit!(model; save_hyperparameter_distribution=false)
     forec = forecast(model, [1 6.0])
     forec.expected_value[1] == [11.0]
 end
