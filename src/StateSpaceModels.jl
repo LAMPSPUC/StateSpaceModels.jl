7a219e3d7ca87b5aee83aaa8f24978a994194c71
andre_ramos <andre_ramos+1-at-806357755127>
2024-10-09 09:43:53 -0400

    add explanatory variables to unobserved_components

--- src/StateSpaceModels.jl
+++ src/StateSpaceModels.jl
@@ -50,6 +50,7 @@ include("models/basicstructural_multivariate.jl")
 include("models/sarima.jl")
 include("models/linear_regression.jl")
 include("models/unobserved_components.jl")
+
 include("models/exponential_smoothing.jl")
 include("models/naive_models.jl")
 include("models/dar.jl")
@@ -87,6 +88,7 @@ export SparseUnivariateKalmanFilter
 export StateSpaceModel
 export UnivariateKalmanFilter
 export UnobservedComponents
+export UnobservedComponentsExplanatory
 export VehicleTracking
 
 # Exported functions
